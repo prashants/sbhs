30b76ce6657f2475c8dfaee97bb2563bf13ba9a9
Prashant P. Shah <pshah.mumbai@gmail.com>
2011-11-25 11:11:08 +0530

    renamed sbhs client script
    Signed-off-by: Prashant P. Shah <pshah.mumbai@gmail.com>

--- sbhsclient.py
+++ sbhsclient.py
@@ -4,11 +4,7 @@ url = "http://10.102.152.29/sbhs/startexp"
 
 data = urllib.urlencode({'rollno' : '11111', 'rno' : '11111'})
 req = urllib2.Request(url)
-try:
-    fd = urllib2.urlopen(req, data)
-    #fd = urllib2.urlopen(req)
-    content = fd.read()
-except urllib2.HTTPError, error:
-    content = error.read()
+fd = urllib2.urlopen(req, data)
+content = fd.read()
 print content
 
