92a3aee903a450f51434dfe20aa24988ee7dd0cd
Prashant P. Shah <pshah.mumbai@gmail.com>
2011-12-19 15:02:52 +0530

    add basic test cases
    Signed-off-by: Prashant P. Shah <pshah.mumbai@gmail.com>

--- sbhshw/tests.py
+++ sbhshw/tests.py
@@ -6,18 +6,36 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test.client import Client
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+    def setUp(self):
+        # every test needs a client
+        self.client = Client()
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+    def test_checkconnection(self):
+        # test connection to server
+        response = self.client.get('/sbhs/checkconnection')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, 'TESTOK')
 
->>> 1 + 1 == 2
-True
-"""}
+    def test_startexp(self):
+        ## test GET request to URL not allowed
+        response = self.client.get('/sbhs/startexp')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, 'Please use the SBHS Client')
 
+        ## test POST request to URL without rollno and password
+        response = self.client.post('/sbhs/startexp')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, '["S", "0", "Please provide username and password"]')
+
+        ## test POST request to URL without rollno
+        response = self.client.post('/sbhs/startexp', {'password' : '1'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, '["S", "0", "Please provide username and password"]')
+
+        ## test POST request to URL without password
+        response = self.client.post('/sbhs/startexp', {'rollno' : '1'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, '["S", "0", "Please provide username and password"]')
