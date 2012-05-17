e68baa0bc1e861f5552f79358f411ca4993a2eb7
Prashant P. Shah <pshah.mumbai@gmail.com>
2012-05-17 14:50:24 +0530

    update web code
    Signed-off-by: Prashant P. Shah <pshah.mumbai@gmail.com>

--- webinterface/sb/config.inc.php
+++ webinterface/sb/config.inc.php
@@ -1,24 +1,24 @@
 <?php
 
 # Database name
-$db = "sbhs";
+$db = "";
 
 # Internet address or hostname of database host
 $db_host = "localhost";
 
 # Database username
-$db_user = "root";
+$db_user = "";
 
 # Database password
-$db_password = "vlabs123";
+$db_password = "";
 
 #Directory that hosts the Website on the Apache Server
 $directory="sb";
 
 //Site for the Url used in activation Mail
-$mailsite = "10.102.152.5";
+$mailsite = "";
 
 //Mail Server for sending mails
-$mailserver = "virtuallab4-desktop.cdeep.org"
+$mailserver = ""
 
 ?>
